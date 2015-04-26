2dc1461fec6c0248f7e886b77b731dc408e8d1f8
med <med@med>
2015-04-26 18:38:18 +0200

    Test

--- src/main/java/dao/Gestion.java
+++ src/main/java/dao/Gestion.java
@@ -4,14 +4,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 import model.Doctor;
-
+//this class allow as ti manage our doctor
 public class Gestion {
+	//list of Doctors
 	List<Doctor> lst;
-
+	//this is our constructer
 	public Gestion() {
 		lst = new ArrayList<Doctor>();
 	}
-
+	//this methode allow as to add a new doctor to the list of doctors
 	public void ajouteDoctor(Doctor d) {
 		lst.add(d);
 	}
